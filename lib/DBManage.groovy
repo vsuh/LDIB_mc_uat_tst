2c69400584351e4ece9bf5ef125cb6cc86c93f02
Valery Sukhikh <sukhikh@moscollector.ru>
2021-09-26 20:29:09 +0300

    wip-3

--- lib/DBManage.groovy
+++ lib/DBManage.groovy
@@ -36,7 +36,7 @@ print "*** got: rasHostnameOrIP ${rasHostnameOrIP}, rasPort ${rasPort}, clusterI
             echo command
        Common.cmd(command)
     }
-  sleep(5)
+
 }
 
 def lockIBbyID(rasHostnameOrIP, rasPort, clusterId, clstAdmin, clstPasswd, databaseId, ibAdmin, ibPwd, lockMessage, lockCode) {
@@ -44,7 +44,7 @@ def lockIBbyID(rasHostnameOrIP, rasPort, clusterId, clstAdmin, clstPasswd, datab
     def TimeNow = Common.TimeNow()
     def NowPlus5min = '20210926T190500'
     if (databaseId != "") {
-        def command = "${env.JN_INSTALLATION_DIR_1C}/rac ${rasHostnameOrIP}:${rasPort}  infobase --cluster ${clusterId} --cluster-user ${clstAdmin} --cluster-pwd ${clstPasswd}  update --infobase=${databaseId}  --infobase-user=${ibAdmin} --infobase-pwd=${ibPwd}  --denied-from=${TimeNow} --denied-to=${NowPlus5min} --permission-code=${lockCode} --denied-message=${lockMessage}  sessions-deny=on --scheduled-jobs-deny=on"
+        def command = "${env.JN_INSTALLATION_DIR_1C}/rac ${rasHostnameOrIP}:${rasPort}  infobase --cluster ${clusterId} --cluster-user ${clstAdmin} --cluster-pwd ${clstPasswd}  update --infobase=${databaseId}  --infobase-user=${ibAdmin} --infobase-pwd=${ibPwd}  --denied-from=${TimeNow} --denied-to=${NowPlus5min} --permission-code=\"${lockCode}\" --denied-message=\"${lockMessage}\"  sessions-deny=\"on\" --scheduled-jobs-deny=\"on\""
 
         if (env.JN_VERBOSE == "true")
             echo command
